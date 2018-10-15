d8dad604edeb606ec9096656ef65ac85093aaab4
CrumrineCoder <crumrinecoding@gmail.com>
2018-10-15 12:22:45 -0400

    Inner Poll registers the data as well.

--- client/src/App/home/poll/components/Poll.js
+++ client/src/App/home/poll/components/Poll.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import { convertFromRaw } from 'draft-js';
-import { stateToHTML } from 'draft-js-export-html';
 
 class Poll extends Component {
 	render() {
-		let html = stateToHTML(convertFromRaw(JSON.parse(this.props.body)));
+		console.log("INNER POLLS", this.props)
 		return (
 			<div>
-				<h1>{this.props.title}</h1>
-				<p id="tag"><b>Tagged: </b>{this.props.tag}</p>
-				<i>{new Date(this.props.createdAt).toDateString()}</i>
-				<div id="pollContent" dangerouslySetInnerHTML={{ __html: html }} />
-				
+				<h1>{this.props.question}</h1>
 			</div >
 		)
 	}
