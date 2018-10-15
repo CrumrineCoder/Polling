dc9afc49bfa146322bec8c2822a60af0856f094d
CrumrineCoder <crumrinecoding@gmail.com>
2018-10-15 12:14:35 -0400

    home page render

--- client/src/App/home/components/PollMini.js
+++ client/src/App/home/components/PollMini.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import { convertFromRaw } from 'draft-js';
-import { stateToHTML } from 'draft-js-export-html';
 import { Link } from 'react-router-dom';
 
 class PollMini extends Component {
 	render() {
-		let html = stateToHTML(convertFromRaw(JSON.parse(this.props.body)));
+		console.log(this.props);
 		return (
 			<div className="pollLink">
-				<Link className="continueReading" to={"posts/" + this.props._id}><h1>{this.props.title}</h1></Link>
-				<p><b>Tagged: </b>{this.props.tag}</p>
-				<Link className="continueReading" to={"posts/" + this.props._id}>Continue Reading <i className="fas fa-long-arrow-alt-right"></i></Link>
+				<Link className="continueReading" to={"polls/" + this.props._id}><h1>{this.props.question}</h1></Link>
+		
 			</div >
 		)
 	}
 
 }
+/*
+		<p><b>Tagged: </b>{this.props.tag}</p>
+				<Link className="continueReading" to={"posts/" + this.props._id}>Continue Reading <i className="fas fa-long-arrow-alt-right"></i></Link>
+				*/
 
 export default PollMini;
 
