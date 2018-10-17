90d85662afd2bad5b62462ba435670da70209558
CrumrineCoder <crumrinecoding@gmail.com>
2018-10-17 17:04:27 -0400

    Radio buttons

--- client/src/App/home/poll/components/Poll.js
+++ client/src/App/home/poll/components/Poll.js
@@ -6,8 +6,16 @@ class Poll extends Component {
 		return (
 			<div>
 				<h1>{this.props.question}</h1>
-				{this.props.answers.map(answer => <div key={answer.text}> {answer.text} </div>)} 
-			</div >
+				{this.props.answers.map(function (answer) {
+					return (
+						<div key={answer.text}>
+							<label>{answer.text}</label>
+							<input type="radio" name="answer" value={answer.text} />
+						</div>
+					)
+				})
+				}
+			</div>
 		)
 	}
 
