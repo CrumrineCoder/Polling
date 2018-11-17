4f435bdcd50553f135452089fee237a617de8a8d
CrumrineCoder <crumrinecoding@gmail.com>
2018-11-17 15:25:57 -0500

    Pie chart

--- client/src/App/result/components/Result.js
+++ client/src/App/result/components/Result.js
@@ -1,50 +1,64 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
-const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
+import { PieChart, Pie, Sector, Cell } from 'recharts';
 
-class SimpleLineChart extends Component{
+const data = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;                    
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+ 	const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+ 
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
+    	{`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+
+class SimplePieChart extends Component{
 	render () {
-		return (
-		  <LineChart width={600} height={300} data={data}
-			  margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-		 <XAxis dataKey="name"/>
-		 <YAxis/>
-		 <CartesianGrid strokeDasharray="3 3"/>
-		 <Tooltip/>
-		 <Legend />
-		 <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-		 <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-		</LineChart>
-	  );
-	}
+  	return (
+    	<PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+        <Pie
+          data={data} 
+          cx={300} 
+          cy={200} 
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80} 
+          fill="#8884d8"
+        >
+        	{
+          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          }
+        </Pie>
+      </PieChart>
+    );
+  }
 }
 
-class Result extends Component {
+	class Result extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = { selected: "", _id: ""	};
+		constructor(props) {
+			super(props);
+			this.state = { selected: "", _id: "" };
+		}
+
+		render() {
+			var { selected } = this.state
+			return (
+				<div>
+					<h1> Hi</h1>
+					<SimplePieChart></SimplePieChart>
+				</div>
+			)
+		}
+
 	}
 
-	render() {
-		var { selected } = this.state
-		return (
-			<div>
-			<h1> Hi</h1>
-			<SimpleLineChart> </SimpleLineChart>
-			</div>
-		)
-	}
-
-}
-
-export default withRouter(Result);
+	export default withRouter(Result);
