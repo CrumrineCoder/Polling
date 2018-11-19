3671fcbb1ef5a6d100082d69b2a29aa6e6e02e29
CrumrineCoder <crumrinecoding@gmail.com>
2018-11-19 11:29:32 -0500

    bar chart works for my data

--- client/src/App/result/components/Result.js
+++ client/src/App/result/components/Result.js
@@ -16,35 +16,32 @@ var renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, perce
 	);
 };
 
-const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
+
 class SimpleBarChart extends Component {
-	render () {
-  	return (
-    	<BarChart width={600} height={300} data={data}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-       <CartesianGrid strokeDasharray="3 3"/>
-       <XAxis dataKey="name"/>
-       <YAxis/>
-       <Tooltip/>
-       <Legend />
-       <Bar dataKey="pv" fill="#8884d8" />
-       <Bar dataKey="uv" fill="#82ca9d" />
-      </BarChart>
-    );
-  }
+	render() {
+		var data = Object.values(this.props);
+		return (
+			<BarChart width={600} height={300} data={data}>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis dataKey="name" />
+				<YAxis />
+				<Tooltip />
+				<Legend />
+				<Bar dataKey="value" fill="#8884d8" >
+					{
+          	data.map((entry, index) => {
+            	const color = entry.value > 4000 ? COLORS[0] : COLORS[1];
+            	return <Cell key={index} fill={color} />;
+            })
+          }
+				</Bar>
+			</BarChart>
+		);
+	}
 }
 
 class SimplePieChart extends Component {
 	render() {
-		console.log("Props", this.props);
 		var data = Object.values(this.props);
 		return (
 			<PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
@@ -83,14 +80,12 @@ class Result extends Component {
 			var insert = { name: fullData[i].text, value: fullData[i].value };
 			data.push(insert);
 		}
-		console.log(data);
-		console.log("PROPS", this.props);
 		var { selected } = this.state
 		return (
 			<div>
 				<h1> Hi</h1>
 				<SimplePieChart {...data} ></SimplePieChart>
-				<SimpleBarChart></SimpleBarChart>
+				<SimpleBarChart {...data}></SimpleBarChart>
 			</div>
 		)
 	}
