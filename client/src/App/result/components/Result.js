0e51cd0e892d5217e28d79d3086026730af88a8f
CrumrineCoder <crumrinecoding@gmail.com>
2018-11-19 11:19:41 -0500

    Sample bar chart added

--- client/src/App/result/components/Result.js
+++ client/src/App/result/components/Result.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { PieChart, Pie, Sector, Cell, Legend } from 'recharts';
+import { PieChart, Pie, Sector, Cell, Legend, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 const RADIAN = Math.PI / 180;
@@ -16,6 +16,31 @@ var renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, perce
 	);
 };
 
+const data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
+class SimpleBarChart extends Component {
+	render () {
+  	return (
+    	<BarChart width={600} height={300} data={data}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+       <CartesianGrid strokeDasharray="3 3"/>
+       <XAxis dataKey="name"/>
+       <YAxis/>
+       <Tooltip/>
+       <Legend />
+       <Bar dataKey="pv" fill="#8884d8" />
+       <Bar dataKey="uv" fill="#82ca9d" />
+      </BarChart>
+    );
+  }
+}
 
 class SimplePieChart extends Component {
 	render() {
@@ -65,6 +90,7 @@ class Result extends Component {
 			<div>
 				<h1> Hi</h1>
 				<SimplePieChart {...data} ></SimplePieChart>
+				<SimpleBarChart></SimpleBarChart>
 			</div>
 		)
 	}
