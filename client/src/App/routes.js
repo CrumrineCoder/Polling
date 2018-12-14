37b48db53a2969c9d2a2151f0ec5223f41f65329
CrumrineCoder <crumrinecoding@gmail.com>
2018-12-14 13:50:08 -0500

    Private Route created, but no authentication yet

--- client/src/App/routes.js
+++ client/src/App/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router";
+import { Route, Switch, Redirect } from "react-router";
 //, Redirect
 //import { withRouter } from 'react-router-dom';
 //import axios from "axios";
@@ -14,33 +14,21 @@ import PageNotFound from "./common/components/PageNotFound";
 import Login from "./home/login/LoginForm";
 import Register from "./home/login/RegisterForm";
 
-/*
-const AuthString = 'Bearer '.concat(USER_TOKEN); 
-axios.get(URL, { headers: { Authorization: AuthString } })
- .then(response => {
-     console.log(response.data);
-  })
- .catch((error) => {
-     console.log('error ' + error);
-  });
-  */
-/*
-
-var isAuth;
-function authenticate() {
- return axios.get("/api/user/auth").then(({ response }) => {
-   return response;
- });
-}
-isAuth = authenticate();
-console.log(isAuth);
-*//*
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    fakeAuth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to={{
+          pathname: '/login',
+          state: { from: props.location }
+        }} />
+  )} />
+)
 
 const fakeAuth = {
-  isAuthenticated: true,
+  isAuthenticated: false,
   authenticate(cb) {
-    console.log("YOU'VE  JUST BEEN AUTHENTICATED!")
-    this.isAuthenticated = true;
+    this.isAuthenticated = true
     setTimeout(cb, 100)
   },
   signout(cb) {
@@ -49,69 +37,14 @@ const fakeAuth = {
   }
 }
 
-class Login extends React.Component {
-  state = {
-    redirectToReferrer: false
-  }
-  login = () => {
-    fakeAuth.authenticate(() => {
-      this.setState(() => ({
-        redirectToReferrer: true
-      }))
-    })
-  }
-  render() {
-    const { from } = this.props.location.state || { from: { pathname: '/' } }
-    const { redirectToReferrer } = this.state
-
-    if (redirectToReferrer === true) {
-      return <Redirect to={from} />
-    }
-
-    return (
-      <div>
-        <p>You must log in to view the page</p>
-        <button onClick={this.login}>Log in</button>
-      </div>
-    )
-  }
-}
-
-const AuthButton = withRouter(({ history }) => (
-  fakeAuth.isAuthenticated ? (
-    <p>
-      Welcome! <button onClick={() => {
-        fakeAuth.signout(() => history.push('/'))
-      }}>Sign out</button>
-    </p>
-  ) : (
-    <p>You are not logged in.</p>
-  )
-))
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      fakeAuth.isAuthenticated === true ? <Component {...props} /> : <Redirect to={{
-        pathname: '/login',
-        state: { from: props.location }
-      }} />
-    }
-  />
-);
-*/
 export default (
-  //  <AuthButton/>
-   // <PrivateRoute path="/polls/results/:id/" component={About} />
-   // <Route path="/login" component={Login} />
   <Switch>  
     <Route exact path="/" component={Home} />
     <Route path="/about" component={About} />
     <Route path="/register" component={Register} />
     <Route path="/login" component={Login} />
     <Route path="/:id/vote/" component={PollShow} />
-    <Route path="/:id/results/" component={Result} />
+    <PrivateRoute path="/:id/results/" component={Result} />
     <Route component={PageNotFound} />
   </Switch>
 );
