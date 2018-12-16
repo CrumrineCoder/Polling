73a3057478728cb05583ba0668c15cb0c217dbf8
CrumrineCoder <crumrinecoding@gmail.com>
2018-12-16 09:12:59 -0500

    Private Route validation works!

--- client/src/App/routes.js
+++ client/src/App/routes.js
@@ -16,57 +16,19 @@ import Login from "./home/login/LoginForm";
 import Register from "./home/login/RegisterForm";
 import { userActions } from '../App/_actions/users.actions.js';
 
-/*
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    fakeAuth.isAuthenticated === true
-      ? <Component {...props} />
-      : <Redirect to={{
-          pathname: '/login',
-          state: { from: props.location }
-        }} />
-  )} />
-) */
-
-
 class PrivateRoute extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      loaded: false,
-      isAuthenticated: false
-    }
-  }
   componentDidMount() {
-    /*   this.authenticate()
-       this.unlisten = this.props.history.listen(() => {
-         Auth.currentAuthenticatedUser()
-           .then(user => console.log('user: ', user))
-           .catch(() => {
-             if (this.state.isAuthenticated) this.setState({ isAuthenticated: false })
-           })
-       }); */
     const { dispatch } = this.props;
-    console.log("JET SET RADIO", dispatch(userActions.getAll()));
+    dispatch(userActions.getAll());
   }
-  componentWillUnmount() {
-    //   this.unlisten()
-  }
-  authenticate() {
-    /*  Auth.currentAuthenticatedUser()
-        .then(() => {
-          this.setState({ loaded: true, isAuthenticated: true })
-        })
-        .catch(() => this.props.history.push('/auth')) */
-  }
+
   render() {
-    console.log("PRIVATE ROUTEPROPS", this.props);
-    console.log("TEST", this.state);
     const { component: Component, ...rest } = this.props;
-    const { loaded, isAuthenticated } = this.state
+    const { didInvalidateCurrentUser, isFetchingCurrentUser } = this.props.users;
     let pageContent = '';
-    if(this.props.users.didInvalidateCurrentUser){
+
+    if(didInvalidateCurrentUser){
       pageContent = (
         (
           <Route {...rest} render={props => (
@@ -77,77 +39,51 @@ class PrivateRoute extends React.Component {
         )
       )
     }
-    else if (this.props.users.isFetchingCurrentUser) {
+
+    else if (isFetchingCurrentUser) {
       pageContent = (
         <div className="pollsLoader">
             Validating...
       	</div>
       )
     } 
+
     else{
       pageContent = (
         (
           <Route {...rest} render={props => (
-            <Component {...props} />
+        
+            <div> <Component {...props} /></div>
         )} />
         )
       )
     }
-    //  const { loaded, isAuthenticated } = this.state
-    // if (!loaded) return null
-    //{this.props.users.items.user.email} 
+
     return (
       <div>{pageContent}</div>
     )
-    /*      <Route
-        {...rest}
-        render={props => {
-          return isAuthenticated ? (
-            <Component {...props} />
-          ) : (
-              <Redirect
-                to={{
-                  pathname: "/auth",
-                }}
-              />
-            )
-        }}
-      /> */
+
   }
 }
 
-//PrivateRoute = withRouter(PrivateRoute)
 function mapStateToProps(state) {
   const { users } = state.home;
-  console.log("MAP STATE USERS", users);
+
   return {
     users
   };
+  
 }
 
 PrivateRoute = withRouter(connect(mapStateToProps)(PrivateRoute));
-/*
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    console.log("THIS.PROPS", this.props);
-    this.isAuthenticated = true
-    setTimeout(cb, 100)
-  },
-  signout(cb) {
-    console.log("THIS.PROPS", this.props);
-    this.isAuthenticated = false
-    setTimeout(cb, 100)
-  }
-}
-*/
+
 export default (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/about" component={About} />
     <Route path="/register" component={Register} />
     <Route path="/login" component={Login} />
-    <PrivateRoute path="/:id/vote/" component={PollShow} />
+    <Route path="/:id/vote/" component={PollShow} />
     <PrivateRoute path="/:id/results/" component={Result} />
     <Route component={PageNotFound} />
   </Switch>
