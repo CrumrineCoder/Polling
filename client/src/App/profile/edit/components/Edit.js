401a0ec4725a818d4f80798433228fbcad605c4a
CrumrineCoder <crumrinecoding@gmail.com>
2019-01-24 17:53:44 -0500

    Basic edit poll form

--- client/src/App/profile/edit/components/Edit.js
+++ client/src/App/profile/edit/components/Edit.js
@@ -8,151 +8,166 @@ import { history } from '../../../store.js';
 
 class Edit extends Component {
 
-	constructor(props) {
-		super(props);
-		const { dispatch } = this.props;
-		// Get the current user, authentication
-		dispatch(userActions.getCurrent());
-		// selected is the votes selected by the user, userAnswer is the user created answer, _parentID is the id of the poll itself, isLoggedIn checks if the user is logged in, choiceType is whether the inputs to select votes are radio (single vote) or checkbox (multiple votes), optionChangeType keeps track of whether we're using multipleOptionChange or optionChange, submitType is  handleSubmit or handleMultipleSubmit, and submissionType is whether the user is voting on a poll answer, a user answer, creating a user answer, or multiple. 
-		this.state = {
-			selected: [],
-			userAnswer: '',
-			_parentID: this.props._id,
-			isLoggedIn: typeof localStorage["user"] !== 'undefined'
-		};
-		// Bind action creators to the state. 
-		this.handleOptionChange = this.handleOptionChange.bind(this);
-		this.handleMultipleOptionChange = this.handleMultipleOptionChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleMultipleSubmit = this.handleMultipleSubmit.bind(this);
-		this.setUserAnswer = this.setUserAnswer.bind(this);
-		this.handleBackClick = this.handleBackClick.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        const { dispatch } = this.props;
+        // Get the current user, authentication
+        dispatch(userActions.getCurrent());
+        // selected is the votes selected by the user, userAnswer is the user created answer, _parentID is the id of the poll itself, isLoggedIn checks if the user is logged in, choiceType is whether the inputs to select votes are radio (single vote) or checkbox (multiple votes), optionChangeType keeps track of whether we're using multipleOptionChange or optionChange, submitType is  handleSubmit or handleMultipleSubmit, and submissionType is whether the user is voting on a poll answer, a user answer, creating a user answer, or multiple. 
+        this.state = {
+            selected: [],
+            userAnswer: '',
+            _parentID: this.props._id,
+            isLoggedIn: typeof localStorage["user"] !== 'undefined',
+            question: '',
+            answers: [
+                { text: '', value: 0 },
+                { text: '', value: 0 }
+            ],
+            options: {
+                MultipleAnswers: false,
+                UserAnswers: false,
+                Rescind: false,
+                SeeResults: false
+            },
+            linked: false,
+            submitted: false
+        };
+        // Bind action creators to the state. 
+        this.handleMultipleOptionChange = this.handleMultipleOptionChange.bind(this);
+        this.handleMultipleSubmit = this.handleMultipleSubmit.bind(this);
+        this.handleChangeField = this.handleChangeField.bind(this);
+        this.handleOptionChange = this.handleOptionChange.bind(this);
+    }
 
-	// Multiple vote submission logic 
-	handleMultipleSubmit() {
-		var { selected, _id, _parentID, isLoggedIn } = this.state;
-		const { dispatch } = this.props;
-		// If the user selected at least one option to vote
-		if (selected !== undefined && selected.length !== 0) {
-			// If the user is logged in 
-			if (isLoggedIn) {
-				let user = JSON.parse(localStorage.getItem('user'));
-				user = user.id;
-				dispatch(pollActions.votePollMultiple({ selected, _id, _parentID, user }))
-			}
-		} // User selected nothing
-		else {
-			alert("You must select at least one option.")
-		}
-	}
+    // Multiple vote submission logic 
+    handleMultipleSubmit() {
+        var { selected, _id, _parentID, isLoggedIn } = this.state;
+        const { dispatch } = this.props;
+        // If the user selected at least one option to vote
+        if (selected !== undefined && selected.length !== 0) {
+            // If the user is logged in 
+            if (isLoggedIn) {
+                let user = JSON.parse(localStorage.getItem('user'));
+                user = user.id;
+                dispatch(pollActions.votePollMultiple({ selected, _id, _parentID, user }))
+            }
+        } // User selected nothing
+        else {
+            alert("You must select at least one option.")
+        }
+    }
 
-	// Multiple vote option change logic
-	handleMultipleOptionChange(evt) {
-		let selectedIndex;
-		var insert = { value: evt.target.value, _id: evt.target.id, submitted: evt.target.getAttribute("submitted") }
-		//	let idIndex;
-		var selected = this.state.selected;
-		//	var _id = this.state._id;
-		// check if the check box is checked or unchecked
-		if (evt.target.checked) {
-			// add the numerical value of the checkbox to options array
-			selected.push(insert);
-			//		_id.push(evt.target.id);
-		} else {
-			// or remove the value from the unchecked checkbox from the array
-			selectedIndex = selected.indexOf(insert);
-			selected.splice(selectedIndex, 1)
-		}
+    // Multiple vote option change logic
+    handleMultipleOptionChange(evt) {
+        let selectedIndex;
+        var insert = { value: evt.target.value, _id: evt.target.id, submitted: evt.target.getAttribute("submitted") }
+        //	let idIndex;
+        var selected = this.state.selected;
+        //	var _id = this.state._id;
+        // check if the check box is checked or unchecked
+        if (evt.target.checked) {
+            // add the numerical value of the checkbox to options array
+            selected.push(insert);
+            //		_id.push(evt.target.id);
+        } else {
+            // or remove the value from the unchecked checkbox from the array
+            selectedIndex = selected.indexOf(insert);
+            selected.splice(selectedIndex, 1)
+        }
 
-		// update the state with the new array of options
-		this.setState({ selected: selected })
-	}
+        // update the state with the new array of options
+        this.setState({ selected: selected })
+    }
 
-	render() {
-		const { isLoggedIn } = this.state;
-		let button;
-		let useranswers = [];
-		let Results;
+    // For Question
+    handleChangeField(key, event) {
+        this.setState({
+            [key]: event.target.value,
+        });
+    }
 
-		// If the user is logged in, they can vote. If not, they'll have to login or register to vote. 
-		if (isLoggedIn) {
-			button = <button onClick={this.state.submitType} className="btn btn-primary float-right">Submit</button>;
-		} else {
-			button = <div className="float-right" id="pleaseLoginOrRegister" > Please  <Link to="/login" >Login</Link> or <Link to="/register">Register</Link> to vote.</div>;
-		}
+    // Add an answer
+    handleAddAnswer = () => {
+        this.setState({
+            answers: this.state.answers.concat([{ text: '', value: 0 }])
+        });
+    }
 
-		// If the poll creator set userAnswers to true
-		if (this.props.options.UserAnswers) {
-			// Build out the user answers
-			// Header
-			useranswers.push(
-				<h4 key="userAnswersHeader">User Answers </h4>
-			)
-			// Already existing user answers
-			for (var i = 0; i < this.props.userAnswers.length; i++) {
-				useranswers.push(
-					<div key={this.props.userAnswers[i]._id}>
-						<input type={this.state.choiceType} name="answer" submitted="userAnswer" submissiontype="userAnswer" onChange={this.state.optionChangeType} value={this.props.userAnswers[i].text} id={this.props.userAnswers[i]._id} className="pollInput" />
-						<label className="pollInputLabel">{this.props.userAnswers[i].text}</label>
-					</div>
-				)
-			}
-			// Make your own user answer
-			useranswers.push(
-				<div key="userAnswersPleaseSpecifyDiv">
-					<input key="userAnswersPleaseSpecifyRadio" type={this.state.choiceType} onChange={this.state.optionChangeType} submitted="toSubmit" name="answer" submissiontype="toSubmit" value={this.state.userAnswer} id="Other" className="pollInput"></input>
-					<input key="userAnswersPleaseSpecifyTextArea" type="text" onChange={this.setUserAnswer} value={this.state.userAnswer} placeholder="Other, please specify" className="pollInputLabel" id="otherSpecify" />
-				</div>
-			)
-		} // Tell the user about userAnswers being set to false
-		 else {
-			useranswers = <p key="userAnswersDisclaimer">User Answers are not allowed for this poll.</p>
-		}
+    // Removve an answer
+    handleRemoveAnswer = (idx) => () => {
+        this.setState({
+            answers: this.state.answers.filter((s, sidx) => idx !== sidx)
+        });
+    }
 
-		// If the poll creator set seeresults to true
-		if (this.props.options.SeeResults) {
-			// Make a button to allow the user to see the results before voting 
-			Results = (<button className="float-left btn btn-secondary" key="seeResultsButton" onClick={this.handleBackClick} value={this.props._id}>Results <i className="fas fa-poll-h"></i></button>);
-		}
+      // For Options
+      handleOptionChange(e) {
+        let options = { ...this.state.options };
+        options[e.target.value] = !options[e.target.value];
+        this.setState({ options });
+    }
 
-		return (
-			<div className="container">
-				<div className="row h-100 justify-content-center align-self-center">
-					<div className="col-md-8">
-						<h1 className="pollVotingSquare">{this.props.question}</h1>
-						<div className="pollVotingSquare">
-							<h4 className="">Answers</h4>
-							{
-								this.props.answers.map(function (answer) {
-									return (
-										<div key={answer._id}>
-											<input type={this.state.choiceType} name="answer" submitted="answer" submissiontype="answer" onChange={this.state.optionChangeType} value={answer.text} id={answer._id} className="pollInput" />
-											<label className="pollInputLabel">{answer.text}</label>
-										</div>
-									)
-								}, this)
-							}
-						</div>
-						<div className="pollVotingSquare">
-							{useranswers}
-						</div>
-						{Results}
-						{button}
-					</div >
-				</div>
-			</div >
-		)
-	}
+     // For Answers
+     handleAnswerTextChange = (idx) => (evt) => {
+        const newAnswers = this.state.answers.map((answer, sidx) => {
+            if (idx !== sidx) return answer;
+            return { ...answer, text: evt.target.value };
+        });
+
+        this.setState({ answers: newAnswers });
+    }
+
+    render() {
+        return (
+            <div className="form">
+                <h1>Edit Poll</h1>
+                <div id="rightPollWrapper">
+                    <div id="rightPoll">
+                        <h4>Options</h4>
+                        <label><input type="checkbox" checked={this.props.options.MultipleAnswers} onChange={this.handleMultipleOptionChange} name="options" value="MultipleAnswers" /> Allow users to select more than one poll answer </label>
+                        <label><input type="checkbox" checked={this.props.options.UserAnswers} onChange={this.handleMultipleOptionChange} name="options" value="UserAnswers" /> Allow users to create poll answers </label>
+                        <label><input type="checkbox" checked={this.props.options.Rescind} onChange={this.handleMultipleOptionChange} name="options" value="Rescind" /> Allow users to rescind their vote </label>
+                        <label><input type="checkbox" checked={this.props.options.SeeResults} onChange={this.handleMultipleOptionChange} name="options" value="SeeResults" /> Allow users to see the results before voting </label>
+                    </div>
+                    <button onClick={this.handleSubmit} className="btn btn-primary float-right" id="pollSubmitButton">Submit</button>
+                </div>
+
+                <div id="leftPoll">
+                    <input
+                        onChange={(ev) => this.handleChangeField('question', ev)}
+                        value={this.props.question}
+                        className="form-control my-3"
+                        placeholder="Poll Question"
+                    />
+
+                    {this.state.answers.map((answer, idx) => (
+                        <div className="answer" key={idx}>
+                            <input
+                                type="text"
+                                placeholder={`Answer #${idx + 1}`}
+                                value={answer.text}
+                                onChange={this.handleAnswerTextChange(idx)}
+                                className="form-control"
+                            />
+                            <button type="button" onClick={this.handleRemoveAnswer(idx)} className="small answerDeleteButton"><i className="fas fa-trash-alt"></i></button>
+                        </div>
+                    ))}
+                    <button type="button" onClick={this.handleAddAnswer} className="small btn btn-secondary float-right" id="addAnswer">Add Answer</button>
+                </div>
+
+            </div>
+        )
+    }
 }
 
 // use the users and votePoll reducers
 function mapStateToProps(state) {
-	const { voting } = state.home.votePoll;
-	const { userInteraction } = state.home.users;
-	return {
-		voting, userInteraction
-	};
+    const { voting } = state.home.votePoll;
+    const { userInteraction } = state.home.users;
+    return {
+        voting, userInteraction
+    };
 }
 
 export default connect(mapStateToProps)(Edit);
