5f490080c5a619d22a232436d20d78925d661eba
CrumrineCoder <crumrinecoding@gmail.com>
2019-02-14 09:10:48 -0500

    Can save a test obj in database

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,25 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
-const cors = require('cors')({origin: true});
+const cors = require('cors')({ origin: true });
 app.use(cors);
+const fire = require("./fire.js");
+var database = fire.database();
 
 app.get('/api', (req, res) => {
-    const date = new Date();
-    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
-    res.json({bongs: 'BONG '.repeat(hours)});
-  });
+  const date = new Date();
+  const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+  res.json({ bongs: 'BONG '.repeat(hours) });
+});
 
-  app.post('/api/polls/createPoll', (req, res) => {
-   console.log(req.body);
-   const date = new Date();
-   const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
-   res.json({bongs: 'BONG '.repeat(hours)});
-  });
+app.post('/api/polls/createPoll', (req, res) => {
+  database.ref('polls/' + "test").set(req.body);
+});
 
 app.get('/', (req, res) => {
-    const date = new Date();
-    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
-    res.send(`
+  const date = new Date();
+  const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+  res.send(`
       <!doctype html>
       <head>
         <title>Time</title>
@@ -33,6 +32,6 @@ app.get('/', (req, res) => {
         <button onClick="refresh(this)">Refresh</button>
       </body>
     </html>`);
-  });
+});
 
-  exports.app = functions.https.onRequest(app);
+exports.app = functions.https.onRequest(app);
