96c14cb3213c6fcae4c1e7019f606dd2675160aa
CrumrineCoder <crumrinecoding@gmail.com>
2019-02-14 03:18:19 -0500

    t

--- functions/index.js
+++ functions/index.js
@@ -2,23 +2,7 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 const cors = require('cors')({origin: true});
-// Get a reference to the database service
-const fire =  require('./fire');
-
 app.use(cors);
-
-app.post("/api/polls/createPoll", (req,res) => {
-  console.log("Preventable Injury", req.body)
-  fire.database().ref('polls/' + req.body.id).set(req.body);
-});
-/*
-(userId, name, email, imageUrl) {
-  firebase.database().ref('users/' + userId).set({
-    username: name,
-    email: email,
-    profile_picture : imageUrl
-  });
-} */
 
 app.get('/api', (req, res) => {
     const date = new Date();
@@ -44,101 +28,4 @@ app.get('/', (req, res) => {
     </html>`);
   });
 
-  app.get('/get/', (req, res, next) => {
-    return Polls.find()
-      .sort({ createdAt: 'descending' })
-      .then((polls) => res.json({ polls: polls.map(poll => poll.toJSON()) }))
-      .catch(next);
-  });
-  
-
-  exports.app = functions.https.onRequest(app); 
-/*
-  const express = require('express');
-const router = express.Router();
-const functions = require('firebase-functions');
-
-router.use('/api', require('../routes/api'));
-
-module.exports = functions.https.onRequest(router);
-*//*
-const functions = require('firebase-functions');
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const cors = require('cors');
-const errorHandler = require('errorhandler');
-const mongoose = require('mongoose');
-const passport = require('passport');
-
-require('dotenv').config();
-
-mongoose.promise = global.Promise;
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-const app = express();
-*/
-// Serve the static files from the React app
-/*
-app.use(cors());
-app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(session({ secret: 'Barrett', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
-// Init passport authentication 
-app.use(passport.initialize());
-// persistent login sessions 
-app.use(passport.session());
-
-
-*/
-/*
-if (!isProduction) {
-	app.use(errorHandler());
-}
-
-const mLab = 'mongodb://' + functions.config().db.user + ':' + functions.config().db.pass +functions.config().db.host + '/' + functions.config().db.name + '?authMode=scram-sha1';
-console.log(mLab);
-mongoose.connect(mLab);
-mongoose.set('debug', true);
-
-// Add models
-require('../models/Polls');
-require('../models/Users');
-require('../config/passport');
-// Add routes
-app.use(require('../routes'));
-
-app.use((req, res, next) => {
-	const err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-
-if (!isProduction) {
-	app.use((req, res, err) => {
-		res.status(err.status || 500);
-
-		res.json({
-			errors: {
-				message: err.message,
-				error: err,
-			},
-		});
-	});
-}
-
-
-// Handles any requests that don't match the ones above
-/*app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/client/build/index.html'));
-}); */
-
-//const port = process.env.PORT || 5000;
-//app.listen(port);
-//exports.app = functions.https.onRequest(app);
-//console.log('App is listening on port ' + port);
+  exports.app = functions.https.onRequest(app);
