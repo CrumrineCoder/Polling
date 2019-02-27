bed7942a603b346ecf91769a9d4fcaaf0ac345f6
CrumrineCoder <crumrinecoding@gmail.com>
2019-02-27 15:32:15 -0500

    Firebase deplosy correctly, rescind doesn't work

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ const fire = require("./fire.js");
 var database = fire.database();
 var auth = fire.auth();
 
-app.get('https://us-central1-polling-269dc.cloudfunctions.net/api/users/current', (req, res, next) => {
+app.get('/api/users/current', (req, res, next) => {
   auth.onAuthStateChanged((user) => {
     if (user) {
       var email = user.email;
@@ -18,12 +18,12 @@ app.get('https://us-central1-polling-269dc.cloudfunctions.net/api/users/current'
   });
 });
 
-app.post('https://us-central1-polling-269dc.cloudfunctions.net/api/users/logout', (req, res) => {
+app.post('/api/users/logout', (req, res) => {
   auth.signOut();
   res.json({ "user": null });
 })
 
-app.post('https://us-central1-polling-269dc.cloudfunctions.net/api/polls/createPoll', (req, res) => {
+app.post('/api/polls/createPoll', (req, res) => {
   var newPostKey = database.ref().child('polls').push().key;
   data = req.body;
   data.id = "uid";
@@ -34,30 +34,26 @@ app.post('https://us-central1-polling-269dc.cloudfunctions.net/api/polls/createP
   res.json(newPostKey);
 });
 
-app.get('https://us-central1-polling-269dc.cloudfunctions.net/api/polls/checkExistence/:question', (req, res) => {
+app.get('/api/polls/checkExistence/:question', (req, res) => {
   var ref = database.ref('polls').orderByChild('question').equalTo(req.params.question)
   ref.once('value', (snapshot) => {
     res.json(snapshot.val() !== null);
   });
 });
 
-app.post('https://us-central1-polling-269dc.cloudfunctions.net/api/polls/editPoll', (req, res) => {
+app.post('/api/polls/editPoll', (req, res) => {
   var ref = database.ref('polls/' + req.body.id);
   ref.update(req.body);
   res.json(req.body.id);
 });
 
-app.post('https://us-central1-polling-269dc.cloudfunctions.net/api/polls/deletePoll/:id', (req, res) => {
+app.post('/api/polls/deletePoll/:id', (req, res) => {
   var ref = database.ref('polls/' + req.params.id);
   ref.remove();
   res.json(req.param.id);
 });
 
-function getKeyByValue(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
-}
-
-app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/rescind/", (req, res) => {
+app.post("/api/polls/rescind/", (req, res) => {
   //for (var i = 0; i < req.body.answersLength; i++) {
   var answersRef = database.ref('polls/' + req.body._parentID + "/answers");
   answersRef.once('value', (snapshot) =>  {
@@ -71,7 +67,7 @@ app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/rescind
           database.ref('polls/' + req.body._parentID + "/answers").child(trueIndex).child("value").transaction( (value) => {
             return (value || 0) - 1;
           });
-          var key = getKeyByValue(childSnapshot.val().users, req.body.user);
+          var key = Object.keys(childSnapshot.val().users).find(key => childSnapshot.val().users[key] === req.body.user);
           database.ref('polls/' + req.body._parentID + "/answers").child(trueIndex).child("users").child(key).remove();
         }
       }
@@ -93,7 +89,7 @@ app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/rescind
             database.ref('polls/' + req.body._parentID + "/userAnswers").child(trueIndex).child("value").transaction((value) => {
               return (value || 0) - 1;
             });
-            var key = getKeyByValue(childSnapshot.val().users, req.body.user);
+            var key = Object.keys(childSnapshot.val().users).find(key => childSnapshot.val().users[key] === req.body.user);
             database.ref('polls/' + req.body._parentID + "/userAnswers").child(trueIndex).child("users").child(key).remove();
           }
         }
@@ -106,7 +102,7 @@ app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/rescind
 });
 
 
-app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/votePollAnswer/", (req, res) => {
+app.post("/api/polls/votePollAnswer/", (req, res) => {
   var databaseRef = database.ref('polls/' + req.body._parentID + "/answers").child(req.body._id).child('value');
   databaseRef.transaction((value) => {
     return (value || 0) + 1;
@@ -116,7 +112,7 @@ app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/votePol
   res.json(req.body._parentID);
 });
 
-app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/voteUserAnswer", (req, res, next) => {
+app.post("/api/polls/voteUserAnswer", (req, res, next) => {
   var databaseRef = database.ref('polls/' + req.body._parentID + "/userAnswers").child(req.body._id).child('value');
   databaseRef.transaction( (value) => {
     return (value || 0) + 1;
@@ -126,7 +122,7 @@ app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/voteUse
   res.json(req.body._parentID);
 })
 
-app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/userVote", (req, res, next) => {
+app.post("/api/polls/userVote", (req, res, next) => {
   var userVote = {
     "text": req.body.userAnswer,
     "value": 1,
@@ -136,7 +132,7 @@ app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/userVot
   res.json(req.body._parentID)
 });
 
-app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/voteMultiple/", (req, res) => {
+app.post("/api/polls/voteMultiple/", (req, res) => {
   for (var i = 0; i < req.body.selected.length; i++) {
     if (req.body.selected[i].submitted === "toSubmit") {
       var userVote = {
@@ -164,7 +160,7 @@ app.post("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/voteMul
   res.json(req.body._parentID);
 });
 
-app.get("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/get/", (req, res) => {
+app.get("/api/polls/get/", (req, res) => {
   var ref = database.ref("polls/");
   var query = ref.orderByChild("question");
   var sum = [];
@@ -176,10 +172,22 @@ app.get("https://us-central1-polling-269dc.cloudfunctions.net/api/polls/get/", (
   });
 });
 
-app.get('https://us-central1-polling-269dc.cloudfunctions.net/api/polls/get/:id', (req, res) => {
+app.get('/api/polls/get/:id', (req, res) => {
   var ref = database.ref('polls/' + req.params.id);
   ref.once('value', (snapshot) => {
     res.json(snapshot.val());
+  });
+});
+
+exports.bigben = functions.https.onRequest((req, res) => {
+  var ref = database.ref("polls/");
+  var query = ref.orderByChild("question");
+  var sum = [];
+  query.once("value", (snap) => {
+    snap.forEach( (childSnap)  => {
+      sum.push(childSnap.val());
+    });
+    res.json(sum);
   });
 });
 
