9d5423b091c3b05d6db0e5cf8eade396f3375079
CrumrineCoder <crumrinecoding@gmail.com>
2019-02-28 06:23:56 -0500

    Results page is responsive

--- client/src/App/result/components/Result.js
+++ client/src/App/result/components/Result.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { PieChart, Pie, Cell, Legend, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { PieChart, Pie, Cell, Legend, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 // Rechart vars
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
@@ -24,48 +24,52 @@ class SimpleBarChart extends Component {
 		var data = Object.values(this.props);
 		//	<Legend layout="vertical" verticalAlign="middle" align="right"/>
 		return (
-			<BarChart width={800} height={400} data={data}>
-				<CartesianGrid strokeDasharray="3 3" />
-				<XAxis dataKey="name" />
-				<YAxis />
-				<Tooltip />
+			<ResponsiveContainer>
+				<BarChart width={800} height={400} data={data}>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis dataKey="name" />
+					<YAxis />
+					<Tooltip />
 
-				<Bar dataKey="votes" fill="#8884d8" >
-					{
-						data.map((entry, index) => {
-							const color = entry.votes > 4000 ? COLORS[0] : COLORS[1];
-							return <Cell key={index} fill={color} />;
-						})
-					}
-				</Bar>
-			</BarChart>
+					<Bar dataKey="votes" fill="#8884d8" >
+						{
+							data.map((entry, index) => {
+								const color = entry.votes > 4000 ? COLORS[0] : COLORS[1];
+								return <Cell key={index} fill={color} />;
+							})
+						}
+					</Bar>
+				</BarChart>
+			</ResponsiveContainer>
 		);
 	}
 }
+
 
 // Pie chart for votes
 class SimplePieChart extends Component {
 	render() {
 		var data = Object.values(this.props);
 		return (
-			<PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
-				<Pie
-					data={data}
-					cx={300}
-					cy={200}
-					labelLine={true}
-					label={renderCustomizedLabel}
-					outerRadius={80}
-					fill="#8884d8"
-					dataKey="votes"
-					isAnimationActive={false}
-				>
-					{
-						data.map((entry, index) => <Cell key={index} fill={COLORS[index % COLORS.length]} />)
-					}
-				</Pie>
-				<Legend layout="vertical" verticalAlign="middle" align="right" />
-			</PieChart>
+			<ResponsiveContainer>
+				<PieChart onMouseEnter={this.onPieEnter}>
+					<Pie
+						data={data}
+						
+						labelLine={true}
+						label={renderCustomizedLabel}
+						outerRadius={80}
+						fill="#8884d8"
+						dataKey="votes"
+						isAnimationActive={false}
+					>
+						{
+							data.map((entry, index) => <Cell key={index} fill={COLORS[index % COLORS.length]} />)
+						}
+					</Pie>
+					<Legend layout="vertical" verticalAlign="middle" align="right" />
+				</PieChart>
+			</ResponsiveContainer>
 		);
 	}
 }
@@ -95,8 +99,12 @@ class Result extends Component {
 		return (
 			<div>
 				<h1>{this.props.votes.question}</h1>
-				<SimpleBarChart {...data}></SimpleBarChart>
-				<SimplePieChart {...data} ></SimplePieChart>
+				<div class="resultChart">
+					<SimpleBarChart {...data}></SimpleBarChart>
+				</div>
+				<div class="resultChart">
+					<SimplePieChart {...data} ></SimplePieChart>
+				</div>	
 			</div>
 		)
 	}
